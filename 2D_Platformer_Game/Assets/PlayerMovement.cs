fdca35bb9229f6595aa836d19ada46e923c04d5f
Nicholas Nazario <Nazario9847@gmail.com>
2020-05-06 15:36:54 -0400

    Mask Dude Animations and Moves

--- 2D_Platformer_Game/Assets/PlayerMovement.cs
+++ 2D_Platformer_Game/Assets/PlayerMovement.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public CharacterController2D controller;
+    public Animator animator;
+    
+    float RUN_SPEED = 80f;
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +21,24 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         
+        horizontalMove = Input.GetAxisRaw("Horizontal")*RUN_SPEED;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+        
+        if(Input.GetButtonDown("Jump")){
+            jump = true;
+        }
+
+        if(Input.GetButtonDown("Crouch")){
+            crouch = true;
+        } else if(Input.GetButtonUp("Crouch")){
+            crouch = false;
+        }
+    }
+
+    // Called a fixed amount of time
+    void FixedUpdate(){
+        // Fixed Delta ensures the player moved the same speed regardless of how often called
+        controller.Move(horizontalMove*Time.fixedDeltaTime, crouch ,jump);
+        jump = false;
     }
 }
